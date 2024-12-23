2980cf861da10b327a73a18a41b56d49016d3ff6
SqualFost <146347981+SqualFost@users.noreply.github.com>
2024-12-23 20:06:28 +0100

    la génération de texte ne marche pas

--- commands/utility/gpt.js
+++ commands/utility/gpt.js
@@ -1,10 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gpt')
-		.setDescription('Génère du texte'),
+		.setDescription('Génère du texte')
+        .addStringOption(option =>
+            option.setName("prompt")
+                .setDescription("Texte envoyé à l'IA.")
+                .setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+        const prompt = interaction.options.getString("prompt");
+        //le prompt retourne bien le texte de l'utilisateur
+        const response = await model.generateContent(prompt);
+        console.log(response);
+        while(response == null){
+            await interaction.editReply("En cours de génération mon copaing");
+        }
+        await interaction.editReply(response);
 	},
 };
